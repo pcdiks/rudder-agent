b9f3c7d304cabcbca06fc7dfd323b079615e122f
Paul-Christiaan Diks <paul-christiaan@startready.com>
2018-08-26 13:04:29 +0200

    Force install and check for root

--- install-rudder-agent.sh
+++ install-rudder-agent.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 wget --quiet -O- "https://www.rudder-project.org/apt-repos/rudder_apt_key.pub" | sudo apt-key add -
 
 echo "deb http://www.rudder-project.org/apt-4.3/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 
-sudo apt-get update
-sudo apt-get install rudder-agent
+apt-get update
+apt-get -y install rudder-agent
 
 echo 'rudder01.home.intra' > /var/rudder/cfengine-community/policy_server.dat
 rudder agent start
